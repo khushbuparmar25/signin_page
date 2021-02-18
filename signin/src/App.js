4d85e7290088e16d0431259a396e3875f8ae325d
khushbuparmar25 <66319265+khushbuparmar25@users.noreply.github.com>
2021-02-18 17:25:45 +0530

    update

--- signin/src/App.js
+++ signin/src/App.js
@@ -1,25 +1,41 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="background background-blur">
-      <div >
-        <h1 className="heading1">Sign In to </h1>
-        <h1 className="heading2"> View Your Profile Direct</h1>
-        <h6 className="subheading">If you don't have an account logged in ever, <a href="sign-up">Signup here!</a></h6>
-      </div>   
-      <div className="img">
-        <img src="/images/pic.png" alt="img"/>
-      </div>   
-      <form >
-        <input className="input_fields1" type="email" placeholder="E-mail/Phone number"></input><br></br><br></br>
-        <input className="input_fields2" type="password" placeholder="Password"></input><br></br><br></br>
-        <button className="signin-button">Sign In</button><br></br><br></br>
-        <div className="extra">---------------Can Also sign in through--------------</div>
-        <div className="google-container"><img src="/images/google.png" className="google" alt="google" width={50} height={30}/></div>
-        <div className="facebook-container" ><img src="/images/facebook.png" className="facebook" alt="facebook"  width={50} height={30}/></div>
-      </form>
-    </div>
+  return (  
+    <div className="container background">
+        <div className="row mobile-view">
+          <div className="col-8">
+            <h1 className="heading1">Sign In to </h1>
+            <h1 className="heading2"> View Your Profile Direct</h1>
+            <h6 className="subheading">If you don't have an account logged in ever, <a href="sign-up">Signup here!</a></h6>
+          </div>
+          <div className="col img img-fluid mobile-view2">
+            <img src="/images/pic.png" alt="img"/>
+          </div>
+          <div className="col-4">
+            <form >
+              <input type="email" className="form-control mb-3 bg-light input_fields1" id="exampleFormControlInput1" placeholder="E-mail/Phone number"/>
+              <input type="password" className="form-control mb-5 bg-light input_fields2" id="inputPassword2" placeholder="Password"/>
+              <button type="submit" className="btn signin-button form-control mb-5">Sign in</button>
+              <div className="mb-5 can"> Can Also sign in through </div>
+              <div className="container icons">
+                <div className="row ">
+                  <div className="col">
+                    <button className="google-container"><img src="/images/google.png" alt="google" width={30} height={25}/></button>
+                  </div>
+                  <div className="col">
+                    <button className="facebook-container" ><img src="/images/facebook.png" alt="facebook"  width={30} height={25}/></button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="col laptop-view img img-fluid">
+            <img src="/images/pic.png" alt="img"/>
+          </div>
+        </div>
+      </div>    
   );
 }
 
